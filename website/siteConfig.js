366a5e89294ea7021392e47e323a73db465939f9
fusionxu <fusionxu@tencent.com>
2019-03-13 21:09:35 +0800

    添加separateCss

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -61,6 +61,7 @@ const siteConfig = {
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
+  separateCss: ["static/api/iOS/css/style.css", "static/api/Android/stylesheet.css"],
   headerLinks: [
     {doc: 'pag-export', label: '文档'},
     {doc: 'api', label: 'API参考'},
