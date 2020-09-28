22560384da7fbc4f1a7c6bcf08e6ac3fff84c4e3
curtisxchen <curtisxchen@tencent.com>
2020-09-28 15:35:23 +0800

    更新URL

--- website/static/javascript/page.js
+++ website/static/javascript/page.js
@@ -91,7 +91,7 @@ docReady(()=>{
         git.className='git-icon';
         footer.children[0].appendChild(git)
         document.getElementById('js_git').onclick = function(){
-            location.href = '//github.com/libpag/libpag.github.io'
+            location.href = '//github.com/libpag/libpag'
         }
 
         //qq group
